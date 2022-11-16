1fdeaf92373182f723e00ed5df359f88a3c070a2
Eliel Martins <elieuprata@gmail.com>
2022-11-16 16:03:11 -0300

    evolution of the course until class 24

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequest from 'App/Exceptions/BadRequestException'
+import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
-    return response.created({})
+    const userPayload = await request.validate(CreateUserValidator)
+
+    const userByEmail = await User.findBy('email', userPayload.email)
+    const userByUsername = await User.findBy('username', userPayload.username)
+
+    if (userByEmail) throw new BadRequest('email already in use', 409)
+    if (userByUsername) throw new BadRequest('username already in use', 409)
+
+    const user = await User.create(userPayload)
+    return response.created({ user })
   }
 }
--- tests/functional/users/users.spec.ts
+++ tests/functional/users/users.spec.ts
@@ -1,23 +1,59 @@
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
-test.group('User', () => {
+test.group('User', (group) => {
   test('it should create an user', async ({ client, assert }) => {
     const userPayload = {
-      email: 'test@test.com',
       username: 'test',
+      email: 'test@test.com',
       password: 'test',
       avatar: 'https://images.com/image/1',
     }
     const response = await client.post('/users').json(userPayload)
 
+    response.assertStatus(201)
+
     assert.exists(response.body().user, 'User undefined')
-    assert.exists(response.body().id, 'Id undefined')
-    assert.equal(response.body().email, userPayload.email)
-    assert.equal(response.body().password, userPayload.password)
-    assert.equal(response.body().avatar, userPayload.avatar)
+    assert.exists(response.body().user.id, 'Id undefined')
+    assert.equal(response.body().user.email, userPayload.email)
+    assert.notExists(response.body().user.password, 'Password defined')
+    assert.equal(response.body().user.avatar, userPayload.avatar)
 
     /* const { password, avatar, ...expected } = userPayload
-    response.assertStatus(201)
+    
     response.assertBodyContains(expected) */
   })
+
+  test('it should return 409 when email is already in use', async ({ client, assert }) => {
+    const { email } = await UserFactory.create()
+    const response = await client.post('/users').json({
+      email,
+      username: 'test',
+      password: 'test',
+    })
+
+    response.assertStatus(409)
+    assert.include(response.body().message, 'email')
+    assert.equal(response.body().code, 'BAD_REQUEST')
+  })
+
+  test('it should return 409 when username is already in use', async ({ client, assert }) => {
+    const { username } = await UserFactory.create()
+    const response = await client.post('/users').json({
+      email: 'test@test.com',
+      username,
+      password: 'test',
+    })
+
+    response.assertStatus(409)
+    assert.include(response.body().message, 'username')
+    assert.equal(response.body().code, 'BAD_REQUEST')
+  })
+
+  test('it should return 422 when required data is not provided', async ({ client, assert }) => {
+    const response = await client.post('/users').json({})
+    console.log(response.body())
+    response.assertStatus(422)
+    assert.equal(response.body().code, 'BAD_REQUEST')
+  }).pin()
 })
